5b67f45abb275b0c5e59e3ff8ffbd30f0b87d056
xDarkHeroHDYTz <ggprocrack901@gmail.com>
2026-08-12 22:47:46 -0300

    ai.nix fix

--- system/ai.nix
+++ system/ai.nix
@@ -19,4 +19,23 @@
       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
     };
   };
+
+  # --- SERVICIO SEARXNG ---
+  services.searx = {
+    enable = true;
+    package = pkgs.searxng;
+    settings = {
+      server = {
+        bind_address = "0.0.0.0";
+        port = 8080;
+        secret_key = "*xy7Yukl0PZIqQK3fZ&padAu";
+      };
+      search = {
+        formats = [
+          "html"
+          "json"
+        ];
+      };
+    };
+  };
 }
